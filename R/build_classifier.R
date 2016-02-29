2bec679f055ef53d5e396833d3499bd0169265ff
Fedorova Anastasia <mayenok@gmail.com>
2016-02-29 16:56:43 +0300

    edited params to be consistent with Trofimov's new clf params and renamed stupidly named vars. resulted call should be like buildClassifier(path, fixation_threshold=500, epoch_size=1000, left_border=-500, channels = c(1:7, 9:12, 14:16), A1=13, A2=14, low=F, high=5, bsln_start = 700, bsln_end = 800, sRate = 500) AND Channels should also be different: right set is  c('PZ','P3','P4','P1','P2','PO7','PO8','PO3','PO4','Oz','O1','O2','POz')

--- R/build_classifier.R
+++ R/build_classifier.R
@@ -6,12 +6,12 @@
 # A1, A2 - idex numbers of reference electrodes !in channels list!
 # low, high - filters
 ###################################################
-buildClassifier <- function(path, fixation_threshold=800, epoch_size=400, 
-                            left_border=150, channels=1:17, A1=16, A2=17, 
-                            low=0.1, high=30, bsln_start = 200, bsln_end = 300)
+buildClassifier <- function(path, fixation_threshold=500, epoch_size=1000, 
+                            left_border=-500, channels=1:17, A1=16, A2=17, 
+                            low=F, high=30, bsln_start = 700, bsln_end = 800, sRate = 500)
   {
-  #@todo: move magick numbers to arguments
-  l <- load_eye_data(path, fixation_threshold, epoch_size, left_border, 500,
+  #channels from Trofimov's clf = c('PZ','P3','P4','P1','P2','PO7','PO8','PO3','PO4','Oz','O1','O2','POz')
+  l <- load_eye_data(path, fixation_threshold, epoch_size, left_border, sRate,
                     channels, A1,A2, low, high)
   
   process_params(l, channels, A1,A2, low, high, bsln_start, bsln_end, left_border)
--- R/prepare_data.R
+++ R/prepare_data.R
@@ -7,10 +7,10 @@ prepare.data <- function(signal, actions, epoch_size, sRate, left_border)
   msgBallClickedInBlockedMode_t <- ceiling(actions[which(actions$Type=="msgBallClickedInBlockedMode"),1]*sRate)
   msgBoardClickedInBlockedMode_t <- ceiling(actions[which(actions$Type=="msgBoardClickedInBlockedMode"),1]*sRate)
   
-  fixationDuration_t <- round((epoch_size/1000) * sRate)
+  epoch_size <- round((epoch_size/1000) * sRate)
   
-  eventsT_t = c(msgbuttonPressed_t, msgballChosen_t, msgBallMoved_t)
-#   eventsT_t = msgbuttonPressed_t
+  #eventsT_t = c(msgbuttonPressed_t, msgballChosen_t, msgBallMoved_t)
+  eventsT_t = msgbuttonPressed_t
   #eventsNT_t = c(msgClickedInBlockMode_t, msgBallClickedInBlockedMode_t, msgBoardClickedInBlockedMode_t)
   eventsNT_t = msgBallClickedInBlockedMode_t
   
@@ -22,17 +22,17 @@ prepare.data <- function(signal, actions, epoch_size, sRate, left_border)
   nChannels <- dim(signal)[2]
   left_border = left_border/1000*sRate
   
-  eegT <- array(dim = c(fixationDuration_t, nChannels-1, length(eventsT_t)))
+  eegT <- array(dim = c(epoch_size, nChannels-1, length(eventsT_t)))
   
   for (i in 1:length(eventsT_t))
   {
-    eegT[ , , i] <- signal[(eventsT_t[i]+left_border+1):(eventsT_t[i]+fixationDuration_t+left_border), -ncol(signal)]
+    eegT[ , , i] <- signal[(eventsT_t[i]+left_border+1):(eventsT_t[i]+epoch_size+left_border), -ncol(signal)]
   }
   
-  eegNT = array(dim = c(fixationDuration_t, nChannels-1, length(eventsNT_t)))
+  eegNT = array(dim = c(epoch_size, nChannels-1, length(eventsNT_t)))
   for (i in 1:length(eventsNT_t))
   {
-    eegNT[ , , i] <- signal[(eventsNT_t[i]+left_border+1):(eventsNT_t[i]+fixationDuration_t+left_border), -ncol(signal)]
+    eegNT[ , , i] <- signal[(eventsNT_t[i]+left_border+1):(eventsNT_t[i]+epoch_size+left_border), -ncol(signal)]
     
   }
   
