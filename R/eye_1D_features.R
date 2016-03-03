3e1d18fa152e5e1b0e9ff75a323469c04fddf0fe
Nastya Fedorova <mayenok@gmail.com>
2016-03-03 16:46:10 +0300

    working on ROC translation from matlab to R

--- R/eye_1D_features.R
+++ R/eye_1D_features.R
@@ -3,7 +3,7 @@ eye_1Dfeats <- function(eegTp, eegNTp, left_border, sRate)
   N0 <- dim(eegNTp)[3]
   N1 <- dim(eegTp)[3]
   
-  nChannels <- dim(eegTp)[2] - 1
+  nChannels <- dim(eegTp)[2]
   t <- dim(eegTp)[1]
   
   beg_time <- left_border / 1000
@@ -13,83 +13,35 @@ eye_1Dfeats <- function(eegTp, eegNTp, left_border, sRate)
   ts_beg <- round((times_beg - beg_time) * sRate);
   ts_end <- round((times_end - beg_time) * sRate);
   
-  #   spec1 <- matrix(data = 0, nrow = t, ncol = nChannels) 
-  #   sens1 <- matrix(data = 0, nrow = t, ncol = nChannels)
-  
   eegTfilt <- eegTp;
   eegNTfilt <- eegNTp;
   
-  X0 <- x <- matrix(nrow = N0, ncol = length(f_channels)*length(ts_beg))
-  X1 <- x <- matrix(nrow = N1, ncol = length(f_channels)*length(ts_beg))
-  
-  #TMP!!!!
-  f_channels <- c(7,8,9,10,11,12,13,14,15,16,17,18,19);
+
+  X0 <- x <- matrix(nrow = N0, ncol = nChannels*length(ts_beg))
+  X1 <- x <- matrix(nrow = N1, ncol = nChannels*length(ts_beg))
   
   for (i in 1:N1) 
   {
-    x <- matrix(nrow = length(ts_beg), ncol = length(f_channels))
+    x <- matrix(nrow = length(ts_beg), ncol = nChannels)
     for (t in 1:length(ts_beg))
     {
-      x[t,] <- colMeans( eegTfilt[ts_beg[t]:ts_end[t], f_channels, i] )
+      x[t,] <- colMeans( eegTfilt[ts_beg[t]:ts_end[t], , i] )
     }
     X1[i,] <- as.vector(x)
   }
   
   for (i in 1:N0) 
   {
-    x <- matrix(nrow = length(ts_beg), ncol = length(f_channels))
+    x <- matrix(nrow = length(ts_beg), ncol = nChannels)
     for (t in 1:length(ts_beg))
     {
-      x[t,] <- colMeans( eegNTfilt[ts_beg[t]:ts_end[t], f_channels, i] )
+      x[t,] <- colMeans( eegNTfilt[ts_beg[t]:ts_end[t], , i] )
     }
     X0[i,] <- as.vector(x)
   }
   
-  ###ended up here translating Trof matlab code
-  
-  
-#   for(tt in 1:t)
-#   {
-#     for(ch in 1:nChannels)
-#     {
-#       X0 = eegNTp[tt, ch, ] 
-#       X1 = eegTp[tt, ch, ]
-#       
-#       X = c(X0, X1)
-#       Y = c(rep(1,N0),  rep(2,N1))
-#       
-#       obj = train_shrinkage(X,Y)
-#       W <- obj$W
-#       
-#       Q0 <- W*X0
-#       Q1 <- W*X1
-#       ths <- c(Q0, Q1) + eps()
-#       ths <- sort(ths) 
-#       
-#       spec <- numeric()
-#       sens <- numeric()
-#       
-#       for (k in 2:length(ths))
-#       {
-#         sens[k] <- length(which(Q1 <= ths[k])) / N1
-#         spec[k] <- length(which(Q0 > ths[k])) / N0
-#         
-#         if(spec[k] < 0.95)
-#         {
-#           idx <- k - 1
-#           break
-#         }
-#       }
-#       
-#       ##########################!!!!!!!!!!!!!!
-#       
-#       spec1[tt, ch] <- spec[idx]
-#       sens1[tt, ch] <- sens[idx]
-#       
-#     }
-#   }
-#   
-#   ss <- list(spec1 = spec1, sens1 = sens1)
-#   
-#   return(ss)
+l <- list(X0 = X0, X1 = X1)
+
+l
+ 
 }
--- R/train_classifier.R
+++ R/train_classifier.R
@@ -3,11 +3,11 @@ train_classifier <- function(eegT, eegNT, fixationDuration, sRate, path, epoch_s
   
   eegTp <- array(dim = c(dim(eegT)[1], dim(eegT)[2], dim(eegT)[3]))
   eegNTp <- array(dim = c(dim(eegNT)[1], dim(eegNT)[2], dim(eegNT)[3]))
-    
+  
   bsln_start = (bsln_start - left_border) / 1000 * sRate;
   bsln_end = (bsln_end - left_border) / 1000 * sRate;
   
-#   bsln_start = max(bsln_start, 1)
+  #   bsln_start = max(bsln_start, 1)
   bsln_end = min(bsln_end, dim(eegT)[1])
   
   
@@ -24,7 +24,83 @@ train_classifier <- function(eegT, eegNT, fixationDuration, sRate, path, epoch_s
     eegNTp[ , , i] <- eeg_baseline_corrected
   }
   
-  spec_sens <- eye_1Dfeats(eegTp, eegNTp, left_border, sRate)
+  X0_X1 <- eye_1Dfeats(eegTp, eegNTp, left_border, sRate)
+  X0 <- X0_X1$X0
+  X1 <- X0_X1$X1
+  
+  #training
+  
+  nfold = 5
+  
+  N0 <-  dim(X0)[1]
+  N1 <- dim(X1)[1]
+  
+  X <- rbind(X0, X1)
+  Y <- c( rep(1, N0), rep(2, N1) )
+  
+  trIdxES <- vector("list", nfold)
+  tstIdxES <- vector("list", nfold)
+  
+  CV <- createFolds(Y, nfold)
+  
+  #preallocate
+  W <- matrix(ncol = nfold, nrow = dim(X1)[2] )
+  th_opt <- vector(mode = "numeric", length = nfold)
+  
+  #generate random samples for training and testing
+  for (i in 1:nfold) {
+    trIdxES[[i]] <- sample( c( rep(0, length(CV[[i]])),
+                               rep(1,  length(Y) - length(CV[[i]]) )
+    ))
+    tstIdxES[[i]] <- as.numeric(!trIdxES[[i]])
+  }
+  
+  for (i in 1:length(CV)) 
+  {
+    trIdx <- trIdxES[[i]]
+    tstIdx <- tstIdxES[[i]]
+    Xtr <- X[which(trIdx==1), ]
+    Xtst <- X[which(tstIdx==1), ]
+    Ytr <- Y[which(trIdx==1)]
+    Ytst <- Y[which(tstIdx==1)]
+    N0tr <- sum(Ytr == 1)
+    N1tr <- sum(Ytr == 2)
+    N0tst <- sum(Ytr == 1)
+    N1tst <- sum(Ytr == 2) 
+    
+    #train
+    obj <- train_shrinkage(Xtr, Ytr)
+    W[,i] <- obj$W
+    
+    #calc threshold using all sample
+    Q <- X %*% W[,i]
+    Q0 <- Q[which(Y==1)] #non target
+    Q1 <- Q[which(Y==2)] #target
+    ths <- Q + .Machine$double.eps
+    ths <- sort(ths)
+    
+    sens_all <- vector()
+    spec_all <- vector()
+    
+    for (k in 1:length(ths))
+    {
+      sens_all[k] <- length(which(Q1 <= ths[k])) / N1
+      spec_all[k] <- length(which(Q0 > ths[k])) / N0
+    }
+    idx <- which(spec_all >= 0.95)
+    idx <- idx[length(idx)]
+    
+    th_opt[i] <- ths[idx]
+    #optimal operating point of the ROC curve
+    roc_obj <- roc(c( rep(1,N1), rep(0, N0) ), c(Q1,Q0))
+    roc_x <- roc_obj$specificities
+    roc_y <- roc_obj$sensitivities
+    
+    ####to_do - ROC coordinates are different in R and in matlab!!!!!!!!! WHYYY!!!
+    
+    #auc calculation stolen from matlab
+    auc <- 0.5 * sum(  ( roc_x[2:length(roc_x)] - roc_x[1:(length(roc_x)-1)] ) * ( roc_y[2:length(roc_y)]  + roc_y[1:(length(roc_y)-1)]))
+  }
   
   Nf <- 110
   
