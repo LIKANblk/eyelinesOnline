401afb3d5dfff4df31088afa5bb2712da51abfcd
Nastya Fedorova <mayenok@gmail.com>
2016-08-23 16:32:17 +0300

    working on classifier response recognition, strange thing with none FN/TP event

--- R/process_file.R
+++ R/process_file.R
@@ -58,6 +58,29 @@ process_file <- function(filename_edf, filename_r2e, file_data) {
     }
   }
   
+  game_state <- rep(NA, length(time))
+  for (i in 1:length(time)) {
+    state_num <- which(time[i] == (game_data$events_timestamps - eyetracking_data$sync_timestamp))
+    if(length(state_num)) {
+      game_state[i] <- state_num
+    }
+  }
+  
+  all_quick_fixations <- eyetracking_messages[grep('quick fixation', eyetracking_messages)]
+  all_quick_fixations_time <- sapply(str_filter(all_quick_fixations, 'time = ([[:digit:]]+)'), function(x) as.numeric(x[[2]]) - eyetracking_data$sync_timestamp)
+  fixation_coords_x <- rep(0, length(time))
+  fixation_coords_y <- rep(0, length(time))
+  for ( i in 1: length(time)) {
+    if(time[i]> 0){
+      fixation_coords_x[i] <- as.numeric(str_filter(
+        all_quick_fixations[sum(all_quick_fixations_time<time[i])],
+        'x = ([[:digit:]]+\\.?[[:digit:]]*)')[[1]][2]) 
+      fixation_coords_y[i] <- as.numeric(str_filter(
+        all_quick_fixations[sum(all_quick_fixations_time<time[i])],
+        'y = ([[:digit:]]+\\.?[[:digit:]]*)')[[1]][2]) 
+    }
+  }
+  
   events <- data.frame(
     time = time,
     field_type = field_type,
@@ -65,8 +88,40 @@ process_file <- function(filename_edf, filename_r2e, file_data) {
     field_position = field_position,
     impossible_move = impossible_move,
     false_alarm = false_alarm,
-    ball_color = ball_color
+    ball_color = ball_color,
+    game_state = game_state,
+    fixation_coords_x = fixation_coords_x,
+    fixation_coords_y = fixation_coords_y
   )
+  
+  toMatch = c('ballRemove','ballCreate','gameOver', 'newGame')
+  events <- events[time>0,]
+  events <- events[-grep(paste(toMatch ,collapse="|"), events$field_type),]
+  
+  if(file_data$record_type == 'test') {
+    
+    true_positives <- eyetracking_messages[grep('received click', eyetracking_messages)]
+    true_positives <- sapply(str_filter(true_positives, 'time = ([[:digit:]]+)'), function(x) as.numeric(x[[2]]) - eyetracking_data$sync_timestamp)
+    true_positives <- true_positives[-which(true_positives > events$time[length(events$time)])]
+    classifier_response <- rep(0, nrow(events))
+    for ( i in 1:length(true_positives)) {
+      if((events$time[min(which(events$time >= true_positives[i]))] - true_positives[i]) < 20) {
+      classifier_response[min(which(events$time >= true_positives[i]))] <- 'true_positive'
+      }
+    }
+    
+    false_negatives <- eyetracking_messages[grep('^fixation in', eyetracking_messages)]
+    false_negatives <- sapply(str_filter(false_negatives, 'time = ([[:digit:]]+)'), function(x) as.numeric(x[[2]]) - eyetracking_data$sync_timestamp)
+    false_negatives <- false_negatives[-which(false_negatives > events$time[length(events$time)])]
+    for ( i in 1:length(false_negatives)) {
+      if((events$time[min(which(events$time >= false_negatives[i]))] - false_negatives[i]) < 20) {
+        classifier_response[min(which(events$time >= false_negatives[i]))] <- 'false_negative'
+      }
+    }
+    
+    events$classifier_response <- classifier_response
+  }
+  
   
   
   list(events = events, file_data = file_data)
