dd9af7d0e710a59d0b5c4b0b3141f5d14db40b4e
Nastya Fedorova <mayenok@gmail.com>
2016-08-29 16:55:46 +0300

    hist outline

--- R/dwell_histogram.R
+++ R/dwell_histogram.R
@@ -1,7 +1,7 @@
 dwell_histogram <- function(events, file_data) {
-  button_dwells_file <- paste(gsub("[[:digit:]]+.r2e", '', file_data$filename_r2e), "button_dwell_hist", "_", str_filter(file_data$filename_r2e, '([[:digit:]]+).r2e')[[1]][1], '.jpg', sep="")
-  field_dwells_file <- paste(gsub("[[:digit:]]+.r2e", '', file_data$filename_r2e), "field_dwell_hist", "_", str_filter(file_data$filename_r2e, '([[:digit:]]+).r2e')[[1]][1], '.jpg', sep="")
   
+  file_to_save <- paste(gsub("[[:digit:]]+.r2e", '', file_data$filename_r2e), "dwell_hist", "_", str_filter(file_data$filename_r2e, '([[:digit:]]+).r2e')[[1]][1], '.jpg', sep="")
+
   p <- ggplot(data=events, aes(x = dwell_time, fill = field_type)) + 
     geom_histogram(bins = (file_data$long_fixation_duration / file_data$delay_between_quick_fixations) -
                      (file_data$quick_fixation_duration / file_data$delay_between_quick_fixations),
@@ -10,19 +10,10 @@ dwell_histogram <- function(events, file_data) {
                      str_filter(file_data$filename_r2e, '([[:digit:]]+).r2e')[[1]][1])) +
     labs(x="Dwell time", y="Count") +
     facet_grid(classifier_response ~ field_type) + 
-    # scale_fill_brewer(palette="Set2") 
-    scale_fill_manual(values=c("#F37748","#067BC2"))
+    scale_fill_brewer(palette="Set2") 
+#     scale_fill_manual(values=c("#F37748","#067BC2"))
   
   print(p)
   ggsave(filename = file_to_save, plot = p)
-  
-  #     pp <- qplot()
-  #     pp <- pp + 
-  #       labs(title=paste("No events ", plot_name ," were found in" , gsub('/.+/',"",filename), "!")) + 
-  #       theme_bw() +
-  #       theme(plot.title = element_text(colour = "red"))
-  #     print(pp)
-  #     ggsave(filename = file_to_save, plot = pp)
-  
   
 }
