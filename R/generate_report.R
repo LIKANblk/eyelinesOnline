e22912d87cbac098f2b7e26d7eed48cea8296a87
Nuzhdin Urii <nuzhdin.urii@gmail.com>
2016-09-28 17:01:56 +0300

    Add generate report

---
+++ R/generate_report.R
@@ -0,0 +1,21 @@
+generate_report <- function(number, folder=NULL){
+  if(is.null(folder)){
+    variants <- c(
+      '~/tmp/newEyelinesOnline/',
+      'd:/YandexDisk/eyelinesOnlineNew/',
+      '~/YandexDisk/eyelinesOnlineNew/'
+    )
+    
+    for(path in variants)
+      if(suppressWarnings(dir.exists(normalizePath(path)))){
+        folder = path
+        break
+      } 
+  }
+  
+  rmarkdown::render(
+    system.file("templates", 'eyelinesReport.Rmd', package="eyelinesOnline"), 
+    params = list(path = file.path(folder, 'data', sprintf('%02i', as.numeric(number)), 'experiment.RData')), 
+    output_file = file.path(folder, sprintf('report%02i.pdf', as.numeric(number)))
+  )
+}
